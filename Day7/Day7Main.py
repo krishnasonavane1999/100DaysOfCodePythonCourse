ea7f097272977e0c9f7e5c1ef1a9adb5f9fee8b2
Krishna Sonavane <53361314+krishnasonavane1999@users.noreply.github.com>
2021-11-21 14:21:44 +0530

    Add files via upload
    Day7
    - Hangman
    - Apply whatever you learnt to build this game
    - interesting stuff
    ✔️

---
+++ Day7/Day7Main.py
@@ -0,0 +1,277 @@
+import random
+import hangman_logo
+import game_over_art
+import hangman_stages
+import you_won_art
+
+print(hangman_logo.logo)
+
+word_list = [
+'abruptly', 
+'absurd', 
+'abyss', 
+'affix', 
+'askew', 
+'avenue', 
+'awkward', 
+'axiom', 
+'azure', 
+'bagpipes', 
+'bandwagon', 
+'banjo', 
+'bayou', 
+'beekeeper', 
+'bikini', 
+'blitz', 
+'blizzard', 
+'boggle', 
+'bookworm', 
+'boxcar', 
+'boxful', 
+'buckaroo', 
+'buffalo', 
+'buffoon', 
+'buxom', 
+'buzzard', 
+'buzzing', 
+'buzzwords', 
+'caliph', 
+'cobweb', 
+'cockiness', 
+'croquet', 
+'crypt', 
+'curacao', 
+'cycle', 
+'daiquiri', 
+'dirndl', 
+'disavow', 
+'dizzying', 
+'duplex', 
+'dwarves', 
+'embezzle', 
+'equip', 
+'espionage', 
+'euouae', 
+'exodus', 
+'faking', 
+'fishhook', 
+'fixable', 
+'fjord', 
+'flapjack', 
+'flopping', 
+'fluffiness', 
+'flyby', 
+'foxglove', 
+'frazzled', 
+'frizzled', 
+'fuchsia', 
+'funny', 
+'gabby', 
+'galaxy', 
+'galvanize', 
+'gazebo', 
+'giaour', 
+'gizmo', 
+'glowworm', 
+'glyph', 
+'gnarly', 
+'gnostic', 
+'gossip', 
+'grogginess', 
+'haiku', 
+'haphazard', 
+'hyphen', 
+'iatrogenic', 
+'icebox', 
+'injury', 
+'ivory', 
+'ivy', 
+'jackpot', 
+'jaundice', 
+'jawbreaker', 
+'jaywalk', 
+'jazziest', 
+'jazzy', 
+'jelly', 
+'jigsaw', 
+'jinx', 
+'jiujitsu', 
+'jockey', 
+'jogging', 
+'joking', 
+'jovial', 
+'joyful', 
+'juicy', 
+'jukebox', 
+'jumbo', 
+'kayak', 
+'kazoo', 
+'keyhole', 
+'khaki', 
+'kilobyte', 
+'kiosk', 
+'kitsch', 
+'kiwifruit', 
+'klutz', 
+'knapsack', 
+'larynx', 
+'lengths', 
+'lucky', 
+'luxury', 
+'lymph', 
+'marquis', 
+'matrix', 
+'megahertz', 
+'microwave', 
+'mnemonic', 
+'mystify', 
+'naphtha', 
+'nightclub', 
+'nowadays', 
+'numbskull', 
+'nymph', 
+'onyx', 
+'ovary', 
+'oxidize', 
+'oxygen', 
+'pajama', 
+'peekaboo', 
+'phlegm', 
+'pixel', 
+'pizazz', 
+'pneumonia', 
+'polka', 
+'pshaw', 
+'psyche', 
+'puppy', 
+'puzzling', 
+'quartz', 
+'queue', 
+'quips', 
+'quixotic', 
+'quiz', 
+'quizzes', 
+'quorum', 
+'razzmatazz', 
+'rhubarb', 
+'rhythm', 
+'rickshaw', 
+'schnapps', 
+'scratch', 
+'shiv', 
+'snazzy', 
+'sphinx', 
+'spritz', 
+'squawk', 
+'staff', 
+'strength', 
+'strengths', 
+'stretch', 
+'stronghold', 
+'stymied', 
+'subway', 
+'swivel', 
+'syndrome', 
+'thriftless', 
+'thumbscrew', 
+'topaz', 
+'transcript', 
+'transgress', 
+'transplant', 
+'triphthong', 
+'twelfth', 
+'twelfths', 
+'unknown', 
+'unworthy', 
+'unzip', 
+'uptown', 
+'vaporize', 
+'vixen', 
+'vodka', 
+'voodoo', 
+'vortex', 
+'voyeurism', 
+'walkway', 
+'waltz', 
+'wave', 
+'wavy', 
+'waxy', 
+'wellspring', 
+'wheezy', 
+'whiskey', 
+'whizzing', 
+'whomever', 
+'wimpy', 
+'witchcraft', 
+'wizard', 
+'woozy', 
+'wristwatch', 
+'wyvern', 
+'xylophone', 
+'yachtsman', 
+'yippee', 
+'yoked', 
+'youthful', 
+'yummy', 
+'zephyr', 
+'zigzag', 
+'zigzagging', 
+'zilch', 
+'zipper', 
+'zodiac', 
+'zombie', 
+]
+
+
+total_words = len(word_list)
+
+pick_random_word_idx = random.randint(0, (total_words - 1))
+
+pick_random_word = word_list[pick_random_word_idx]
+
+# print("Randomly picked word: " + pick_random_word)
+
+pick_random_word_len = len(pick_random_word)
+
+# print("Randomly picked word length: ", pick_random_word_len)
+
+char_to_guess = ''
+dashed_word = list("")
+hangman = 0
+
+for i in range(pick_random_word_len):
+  dashed_word += '_'
+
+traverse = pick_random_word_len
+
+print(''.join(dashed_word))
+print(pick_random_word)
+
+while traverse:
+  char_to_guess = input("Guess the character: ")
+  is_char_persent = -1
+  for i in range(pick_random_word_len):
+    if char_to_guess == pick_random_word[i]:
+      dashed_word[i] = char_to_guess
+    else:
+      is_char_persent = -1
+
+  if is_char_persent == -1:
+    hangman += 1
+
+  print(''.join(dashed_word))
+    
+  if hangman == 7:
+    print(hangman_stages.stages[-hangman])
+    print(game_over_art.game_over)
+    break
+  elif is_char_persent == -1:
+    print(hangman_stages.stages[-hangman])
+    print("Hangman: ",hangman)
+  
+  if ''.join(dashed_word).find('_') == -1:
+    print(you_won_art.you_won)
+  
+
+traverse -=1
+
---
+++ Day7/start.md
@@ -0,0 +1,4 @@
+Day7
+- Hangman
+- Apply whatever you learnt to build this game
+- interesting stuff
