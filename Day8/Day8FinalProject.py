c834dd90812d716366d383fbabc35424467c4991
Krishna Sonavane <53361314+krishnasonavane1999@users.noreply.github.com>
2021-11-22 23:57:12 +0530

    Add files via upload
    Day8
    - functions with inputs
    - positional vs keyword arguments
    - arguments vs parameters
    - cool exercises
    - final caeser cipher encryption project
    ✔️

---
+++ Day8/Day8FinalProject.py
@@ -0,0 +1,53 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+print(logo)
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def ciser_cipher(direction, plain_text, shift_amount):
+  finalResult = ""
+  finalIdx  = 0
+  for ch in text:
+    for i in range(0, 26):
+      if ch == alphabet[i]:
+        finalIdx =  shift + int(i)
+        if direction == "encode":
+          if finalIdx > 25:
+            finalIdx -= 26
+            finalResult += alphabet[finalIdx]
+            finalIdx = 0
+          else:
+            finalResult += alphabet[finalIdx]
+            finalIdx = 0
+        elif direction == "decode":
+          finalIdx =  int(i) - shift
+          finalResult += alphabet[finalIdx]
+          finalIdx = 0
+        else:
+          print("Please select the valid choice!!")
+
+
+      
+  print(finalResult)
+
+
+ciser_cipher(direction, text, shift)
---
+++ Day8/start.md
@@ -0,0 +1,6 @@
+Day8
+- functions with inputs
+- positional vs keywordd argumentts
+- arguments vs parameters
+- cool exercises
+- final caeser cipher encryption project
